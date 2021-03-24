11536d86862112a1789d4800e3953fc58a698b7f
Gaurav <gauravkapadiya8@gmail.com>
2021-03-24 21:06:51 +0530

    fix for import issue

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react", "@babel/runtime"],
+  presets: ["@babel/preset-env", "@babel/preset-react"],
   ignore: [
     /\/node_modules\//,
   ]
--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React from "react";
 import "./App.css";
-import ColorPallete from "./components/ColorPallete";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef(null);
+  }
+
+  generateRGB() {
+    const colors = [];
+    for (let i = 0; i < 256; i += 8) {
+      for (let j = 0; j < 256; j += 8) {
+        for (let k = 0; k < 256; k += 8) {
+          colors.push("rgba(" + i + "," + j + "," + k + "," + 1 + ")");
+        }
+      }
+    }
+    return colors;
+  }
+
+  draw(colors) {
+    const canvas = this.canvasRef.current;
+    let ctx = canvas.getContext("2d");
+    let count = 0;
+    for (let i = 0; i < 256; i++) {
+      for (let j = 0; j < 128; j++) {
+        ctx.fillStyle = colors[count++];
+        ctx.fillRect(i + 1, j + 1, 1, 1);
+      }
+    }
+  }
+
+  componentDidMount() {
+    let colors = this.generateRGB();
+    this.draw(colors);
+  }
+
   render() {
     return (
       <div className="App">
         <h3 className="title">Color Pallete</h3>
         <div className="color-pallete-container">
-            <ColorPallete />
+          <canvas
+            id="canvas"
+            ref={this.canvasRef}
+            width="256"
+            height="128"
+          ></canvas>
         </div>
       </div>
     );
